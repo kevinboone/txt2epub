1b4d443f794767a172a004d125bccaac916725eb
Ken H <60443687+KenH2000@users.noreply.github.com>
2023-05-21 10:29:17 -0700

    Update text.h
    11c11
    <         BOOL line_paras, BOOL remove_pagenum);
    ---
    >         BOOL line_paras, BOOL remove_pagenum, BOOL para_indent);

--- src/text.h
+++ src/text.h
@@ -8,7 +8,6 @@ Copyright (c)2017 Kevin Boone, GPLv3.0
 
 char *text_file_to_xhtml (const char *textfile, const char *title,
         BOOL indent_is_para, BOOL markdown, BOOL first_is_title, 
-        BOOL line_paras, BOOL remove_pagenum);
+        BOOL line_paras, BOOL remove_pagenum, BOOL para_indent);
 void text_init_regex (void);
 void text_cleanup_regex (void);
-
